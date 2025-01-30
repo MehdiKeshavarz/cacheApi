161b633e0493d1656e104d2e125325718d7d3ad2
mehdiKeshavarz <mehdi2019keshavarz@gmail.com>
2025-01-30 10:07:11 +0330

    implement cacheService logic

---
+++ src/main/java/com/example/cacheApi/services/CacheService.java
@@ -0,0 +1,51 @@
+package com.example.cacheApi.services;
+
+
+import com.example.cacheApi.dto.CacheData;
+import com.example.cacheApi.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class CacheService {
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    @Autowired
+    public CacheService(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    public void create(CacheData data) {
+        if (data.getKey() == null || data.getValue() == null) {
+            throw new IllegalArgumentException("Key and Value are required");
+        }
+
+        redisTemplate.opsForValue().set(
+                data.getKey(),
+                data.getValue(),
+                data.getTimeToLiveInMinute(),
+                TimeUnit.MINUTES);
+    }
+
+    public String read(String key) {
+        String value = (String) redisTemplate.opsForValue().get(key);
+
+        if (value == null) {
+            throw new ResourceNotFoundException("No data found in the cache for key " + key + "!");
+        }
+
+        return value;
+    }
+
+    public void delete(String key) {
+        Boolean deleted = redisTemplate.delete(key);
+
+        if (!deleted) {
+            throw new ResourceNotFoundException("No data found in the cache for key " + key + "!");
+        }
+
+    }
+}
